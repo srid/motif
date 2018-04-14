aac6711ccd2dc8c821a24675dd06fc850c929776
Sridhar Ratnakumar <srid@srid.ca>
2018-04-14 11:28:31 -0400

    Use jsaddle-warp by default
    Need to disable this when building for Android

--- default.nix
+++ default.nix
@@ -23,4 +23,10 @@
     ghc = ["common" "backend" "frontend"];
     ghcjs = ["common" "frontend"];
   };
+
+  overrides = self: super: { 
+    # Comment this out when building for Android. 
+    # https://github.com/reflex-frp/reflex-platform/pull/281#discussion_r181538045
+    reflex-dom = pkgs.haskell.lib.addBuildDepend (pkgs.haskell.lib.enableCabalFlag super.reflex-dom "use-warp") self.jsaddle-warp;
+  };
 })
