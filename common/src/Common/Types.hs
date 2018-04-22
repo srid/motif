4e25194600c0f8411ceaa397bba3f17aa7fefadf
Sridhar Ratnakumar <srid@srid.ca>
2018-04-22 13:37:19 -0400

    Use show/read instead of JSON

--- common/src/Common/Types.hs
+++ common/src/Common/Types.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 module Common.Types where
 
 import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.ByteString.Lazy.Char8 as BC
 import Data.Default
+import Data.Monoid ((<>))
 import Data.String (IsString (fromString))
 import Data.Text (Text)
 import Data.Tree
 import Data.Typeable
 import Data.UUID (UUID)
 import GHC.Generics
+import Network.HTTP.Media ((//), (/:))
+import Text.Read (readMaybe)
 
 import qualified Data.Text as T
 import Servant.API
@@ -25,7 +31,7 @@ data NodeState = NodeState
   { _nodeStateDummy :: Bool
   , _nodeStateOpen :: Bool
   }
-  deriving (Generic, Eq, Ord, Show, ToJSON, FromJSON)
+  deriving (Generic, Eq, Ord, Show, Read, ToJSON, FromJSON)
 
 type MotifTree a = Tree (UUID, NodeState, a)
 
@@ -35,18 +41,19 @@ instance Default NodeState where
 newtype Motif = Motif
   { _motifTree :: MomentTree
   }
-  deriving (Generic, Show, Typeable, ToJSON, FromJSON)
+  deriving (Generic, Show, Read, Typeable, ToJSON, FromJSON)
 
 newtype MomentTree = MomentTree { unMomentTree :: MotifTree Moment }
-  deriving (Generic, Eq, Show, ToJSON, FromJSON)
+  deriving (Generic, Eq, Show, Read, ToJSON, FromJSON)
 
+-- FIXME: GADT is more appropriate here.
 data Moment
   = MomentInbox Content  -- ^ Simplest content type; just text.
   | MomentJournal [Context] Content
-  deriving (Generic, Eq, Ord, Show, ToJSON, FromJSON)
+  deriving (Generic, Eq, Ord, Show, Read, ToJSON, FromJSON)
 
 newtype Content = Content { unContent :: Text }
-  deriving (Generic, Eq, Ord, Show, ToJSON, FromJSON)
+  deriving (Generic, Eq, Ord, Show, Read, ToJSON, FromJSON)
 
 instance IsString Content where
   fromString = Content . T.pack
@@ -56,7 +63,7 @@ data Context
   | ContextChore
   | ContextReading
   | ContextIdea
-  deriving (Generic, Eq, Ord, Show, ToJSON, FromJSON)
+  deriving (Generic, Eq, Ord, Show, Read, ToJSON, FromJSON)
 
 class IsMoment a where
   getContext :: a -> [Context]
@@ -79,10 +86,24 @@ data MotifAction
   | MotifActionAddToInbox Text
   | MotifActionDelete UUID
   | MotifActionSetNodeState UUID NodeState
-  deriving (Generic, Eq, Show, Ord, ToJSON, FromJSON)
+  deriving (Generic, Eq, Show, Read, Ord, ToJSON, FromJSON)
 
-type MotifAPI =
-  "motif" :> ReqBody '[JSON] MotifAction :> Post '[JSON] (Either Text Motif)
+type MotifAPI = "motif"
+  :> ReqBody '[HaskellType] MotifAction
+  :> Post '[HaskellType] (Either Text Motif)
+
+data HaskellType
+
+instance Accept HaskellType where
+  contentType _ = "text" // "haskell" /: ("charset", "utf-8")
+
+instance Show a => MimeRender HaskellType a where
+  mimeRender _ = BC.pack . show
+
+instance Read a => MimeUnrender HaskellType a where
+  mimeUnrender _ = f . BC.unpack
+    where
+      f s = maybe (Left ("Bad text: " <> s)) Right $ readMaybe s
 
 --------------------
 --- Future types!
