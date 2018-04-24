fc5284dde080d6830aabaa4185aefea78630000b
Sridhar Ratnakumar <srid@srid.ca>
2018-04-24 19:12:12 -0400

    Fix service.nix, and add deployment instruction

--- README.md
+++ README.md
@@ -5,14 +5,29 @@
 ## Development
 
 ```
-./ghcid-frontend
+./ghcid-frontend  # Runs jsaddle-warp
 ```
 
 ```
 ./ghcid-backend
 ```
 
-Visit http://localhost:3000/
+Visit http://localhost:3003/
 
-## Building
+## Deployment on NixOS
 
+Clone this repo and add the full path to the `service.nix` file to the imports section of your /etc/nixos/configuration.nix (you may alternatively use [home-manager](https://github.com/rycee/home-manager) should you wish to keep it all user-local).
+
+Run `sudo nixos-rebuild switch`. This will add an *user* systemd unit called `motifapp`. You will need to launch it manually using:
+
+```
+systemctl --user start motifapp
+```
+
+Then visit http://localhost:9001/index.html in the browser.
+
+Note that the server is hardcoded to use `~/Dropbox/Apps/motifdb` as the location of the (acid-state) database.
+
+## Roadmap
+
+See https://github.com/srid/motif/projects/1
--- service.nix
+++ service.nix
@@ -1,21 +1,19 @@
 { ... }:
 
-# Launch an user systemd unit, storing database in Dropbox.
-# Port 9001 is hardcoded, with the intention of reserving 90XX 
-# for my apps.
+# Launch an *user* systemd unit, storing database in Dropbox.
+# Port 9000 is hardcoded, with the intention of reserving 90XX 
+# for my personal apps.
 {
-  systemd.user.services.motif = 
+  systemd.user.services.motifapp = 
     let 
-      motif = import ./default.nix { };
-      backend = motif.ghc.backend;
-      frontend = motif.ghcjs.frontend;
+      motif = import ./release.nix;
     in {
       description = "Motif app";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
       serviceConfig = 
-        { ExecStart = "${backend}/bin/backend 9001 %h/Dropbox/Apps/motifdb";
-          WorkingDirectory = "${frontend}/bin/frontend.jsexe";
+        { ExecStart = "${motif}/backend 9000 %h/Dropbox/Apps/motifdb";
+          WorkingDirectory = "${motif}";
         };
     };
 }
